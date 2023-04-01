eb7aaf0d55ba4bd1b43ad6b57cebca2b472664f1
toncha <nata.toncha@mail.ru>
2023-04-01 22:35:36 +0300

    Nt6java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14260;
+        int limm = 20;
+        int kmil = 0;
+        if (price >= limm) {
+            kmil = price / limm;
+        }
+        System.out.println("Количество миль " + kmil);
+
+    }
+}
